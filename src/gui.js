def3ef63cf6d5f929568790b6e1f40d72ecf4c51
Harry Phillips <phillipsharry86@gmail.com>
2015-03-24 15:51:45 +0000

    attempt to fix mixed tabs and spaces

--- src/gui.js
+++ src/gui.js
@@ -88,20 +88,20 @@ window.define(['config', './util', './events'], function (config, util, events) 
             faurl = "//maxcdn.bootstrapcdn.com/font-awesome/" +
             "4.3.0" +
             "/css/font-awesome.min.css",
-			
-			// attach gui element and publish loaded event
-			publish = function () {
-				// attach gui when styles have loaded
-				document.body.appendChild(self.tree.main.element);
-				util.log("debug", "+ attached gui tree");
+            
+            // attach gui element and publish loaded event
+            publish = function () {
+                // attach gui when styles have loaded
+                document.body.appendChild(self.tree.main.element);
+                util.log("debug", "+ attached gui tree");
 
-				// set the initialised flag
-				self.inited = true;
+                // set the initialised flag
+                self.inited = true;
 
-				// gui is always last to load - publish loaded event
-				util.log("debug", "+ publishing 'kbs/loaded'");
-				events.publish("kbs/loaded");
-			};
+                // gui is always last to load - publish loaded event
+                util.log("debug", "+ publishing 'kbs/loaded'");
+                events.publish("kbs/loaded");
+            };
 
         // props
         mainlink.rel = "stylesheet";
@@ -118,25 +118,25 @@ window.define(['config', './util', './events'], function (config, util, events) 
         
         themelink.onload = function () {
             util.log("okay", "+ theme.css loaded");
-			
-			if (config.offline) {
-				publish();
-			}
+            
+            if (config.offline) {
+                publish();
+            }
         };
         
         // this will be last to load - attach the exec fn
         falink.onload = function () {
             util.log("okay", "+ font-awesome.css loaded");
-			
-			if (!config.offline) {
-				publish();
-			}
+            
+            if (!config.offline) {
+                publish();
+            }
         };
         
         // write out to document
-		if (!config.offline) {
-			document.head.appendChild(falink);
-		}
+        if (!config.offline) {
+            document.head.appendChild(falink);
+        }
         document.head.appendChild(mainlink);
         document.head.appendChild(themelink);
 
@@ -204,16 +204,16 @@ window.define(['config', './util', './events'], function (config, util, events) 
     GUI.Console = function (gui) {
         this.tree = this.buildNodeTree(gui);
     };
-	
-	// write log to console
-	GUI.Console.prototype.write = function (args) {
+    
+    // write log to console
+    GUI.Console.prototype.write = function (args) {
         // get nodes using the self pointer!
         var out = self.tree.main.conswrap.cons.out.element,
             log = new Node("div", "kbs-log-node kbs-" + args.type),
             content = args.msg,
-			i = 0;
-		
-		// object node
+            i = 0;
+        
+        // object node
         if (args.obj) {
             content += "<pre class='kbs-object'>";
             content += args.obj + "</pre>";
@@ -226,10 +226,10 @@ window.define(['config', './util', './events'], function (config, util, events) 
         
         // refresh
         self.refresh();
-	};
-	
-	// clear console
-	GUI.Console.prototype.clear = function () {
+    };
+    
+    // clear console
+    GUI.Console.prototype.clear = function () {
         var cons = this.tree.cons.element,
             out = this.tree.cons.out.element,
             start = new Date().getTime(),
@@ -249,35 +249,35 @@ window.define(['config', './util', './events'], function (config, util, events) 
         // bench
         end = new Date().getTime() - start;
         util.log("okay", "cleared all logs in " + end + " ms");
-	};
-	
-	// close console
-	GUI.Console.prototype.close = function () {
-		var element = this.tree.element,
-			classes = element.className;
-		
-		element.className = classes += " kbs-close";
-	};
-	
-	// open console
-	GUI.Console.prototype.open = function () {
-		var element = this.tree.element,
-			classes = element.className;
-		
-		element.className = classes.replace(" kbs-close", "");
-	};
-	
-	// destroy the console instance
-	GUI.Console.prototype.destroy = function () {
-		var confirm = window.prompt("Are you sure you want to destroy the " +
-						"console instance? You will have to refresh the page " +
-									"to recover it.",
-					  "Click OK or CANCEL - input not needed");
-		
-		if (confirm) {
-			self.tree.main.element.removeChild(self.tree.main.conswrap.element);
-		}
-	};
+    };
+    
+    // close console
+    GUI.Console.prototype.close = function () {
+        var element = this.tree.element,
+            classes = element.className;
+        
+        element.className = classes += " kbs-close";
+    };
+    
+    // open console
+    GUI.Console.prototype.open = function () {
+        var element = this.tree.element,
+            classes = element.className;
+        
+        element.className = classes.replace(" kbs-close", "");
+    };
+    
+    // destroy the console instance
+    GUI.Console.prototype.destroy = function () {
+        var confirm = window.prompt("Are you sure you want to destroy the " +
+                "console instance? You will have to refresh the page " +
+                "to recover it.",
+                "Click OK or CANCEL - input not needed");
+        
+        if (confirm) {
+            self.tree.main.element.removeChild(self.tree.main.conswrap.element);
+        }
+    };
     
     // create a new console tool
     GUI.Console.prototype.createTool = function (toolbar, tool) {
@@ -332,28 +332,28 @@ window.define(['config', './util', './events'], function (config, util, events) 
         // toggle tool
         this.createTool(constools, "toggle").element.onclick = function () {
             var classes = wrapper.element.className,
-				closed = classes.indexOf("kbs-close") !== -1,
-				full = classes.indexOf("kbs-full") !== -1;
+                closed = classes.indexOf("kbs-close") !== -1,
+                full = classes.indexOf("kbs-full") !== -1;
 
-			// if not closed and not full screen
+            // if not closed and not full screen
             if (!closed && !full) {
-				// make full screen
+                // make full screen
                 wrapper.element.className += " kbs-full";
             }
-			
-			// if in full screen
-			if (full) {
-				// shrink
-				wrapper.element.className =
-					wrapper.element.className.replace(" kbs-full", "");
-			}
-			
-			// if closed
-			if (closed) {
-				// open
-				wrapper.element.className =
-					wrapper.element.className.replace(" kbs-close", "");
-			}
+            
+            // if in full screen
+            if (full) {
+                // shrink
+                wrapper.element.className =
+                    wrapper.element.className.replace(" kbs-full", "");
+            }
+            
+            // if closed
+            if (closed) {
+                // open
+                wrapper.element.className =
+                    wrapper.element.className.replace(" kbs-close", "");
+            }
         };
 
         // clear tool
