2208aa5400afc20f34a36a14c4d64e051c0d5084
Harry Phillips <phillipsharry86@gmail.com>
2015-10-22 12:28:06 +0100

    Fix stupid letter casing error

--- addon/defaults/preferences/prefs.js
+++ addon/defaults/preferences/prefs.js
@@ -1,2 +1,2 @@
-pref("extensions.kanban.baseUrl", "https://cdn.rawgit.com/HarryPhillips/Bugherd-Kanban/v1.5.1/");
+pref("extensions.kanban.baseUrl", "https://cdn.rawgit.com/HarryPhillips/BugHerd-Kanban/v1.5.1/");
 pref("extensions.kanban.useDist", true);
